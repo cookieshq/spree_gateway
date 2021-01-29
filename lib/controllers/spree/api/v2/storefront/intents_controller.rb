d9af7f31586d11bef7b140c82d59d4a78efbdebf
Sunny Asar <asarsunny@gmail.com>
2021-01-29 12:37:46 +0100

    intent migration to v1

--- lib/controllers/spree/api/v2/storefront/intents_controller.rb
+++ lib/controllers/spree/api/v2/storefront/intents_controller.rb
@@ -2,8 +2,8 @@ module Spree
   module Api
     module V2
       module Storefront
-        class IntentsController < ::Spree::Api::V2::BaseController
-          include Spree::Api::V2::Storefront::OrderConcern
+        class IntentsController < ::Spree::Api::BaseController
+          # include Spree::Api::V2::Storefront::OrderConcern
 
           def handle_response
             if params['response']['error']
@@ -14,12 +14,66 @@ module Spree
             end
           end
 
+          def render_serialized_payload(status = 200)
+            render json: yield, status: status, content_type: content_type
+          rescue ArgumentError => exception
+            render_error_payload(exception.message, 400)
+          end
+
+          def render_error_payload(error, status = 422)
+            if error.is_a?(Struct)
+              render json: { error: error.to_s, errors: error.to_h }, status: status, content_type: content_type
+            elsif error.is_a?(String)
+              render json: { error: error }, status: status, content_type: content_type
+            end
+          end
+
           private
 
           def invalidate_payment
             payment = spree_current_order.payments.find_by!(response_code: params['response']['error']['payment_intent']['id'])
             payment.update(state: 'failed', intent_client_key: nil)
           end
+
+          # private methods from include Spree::Api::V2::Storefront::OrderConcern
+
+          def render_order(result)
+            if result.success?
+              render_serialized_payload { serialized_current_order }
+            else
+              render_error_payload(result.error)
+            end
+          end
+
+          def ensure_order
+            raise ActiveRecord::RecordNotFound if spree_current_order.nil?
+          end
+
+          def order_token
+            request.headers['X-Spree-Order-Token'] || params[:order_token]
+          end
+
+          def spree_current_order
+            @spree_current_order ||= find_spree_current_order
+          end
+
+          def find_spree_current_order
+            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
+              store: current_store,
+              user: spree_current_user,
+              token: order_token,
+              currency: current_currency
+            )
+          end
+
+          def serialize_order(order)
+            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
+          end
+
+          def serialized_current_order
+            serialize_order(spree_current_order)
+          end
+
         end
       end
     end

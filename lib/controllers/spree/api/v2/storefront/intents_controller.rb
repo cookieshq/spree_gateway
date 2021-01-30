36c5b3f6f4c324bb10f26b5bfcc7d22e3f60c1b4
Sunny Asar <asarsunny@gmail.com>
2021-01-30 09:21:04 +0100

    removed unused methods

--- lib/controllers/spree/api/v2/storefront/intents_controller.rb
+++ lib/controllers/spree/api/v2/storefront/intents_controller.rb
@@ -34,46 +34,6 @@ module Spree
             payment = spree_current_order.payments.find_by!(response_code: params['response']['error']['payment_intent']['id'])
             payment.update(state: 'failed', intent_client_key: nil)
           end
-
-          # private methods from include Spree::Api::V2::Storefront::OrderConcern
-
-          def render_order(result)
-            if result.success?
-              render_serialized_payload { serialized_current_order }
-            else
-              render_error_payload(result.error)
-            end
-          end
-
-          def ensure_order
-            raise ActiveRecord::RecordNotFound if spree_current_order.nil?
-          end
-
-          def order_token
-            request.headers['X-Spree-Order-Token'] || params[:order_token]
-          end
-
-          def spree_current_order
-            @spree_current_order ||= find_spree_current_order
-          end
-
-          def find_spree_current_order
-            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
-              store: current_store,
-              user: spree_current_user,
-              token: order_token,
-              currency: current_currency
-            )
-          end
-
-          def serialize_order(order)
-            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
-          end
-
-          def serialized_current_order
-            serialize_order(spree_current_order)
-          end
-
         end
       end
     end
